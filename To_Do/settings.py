d21197d988a3616571f0df994ea32dcfe76f564b
Preemalobo <preemalobo2016@gmail.com>
2025-02-12 20:33:27 +0530

    Included GitIgnore

--- To_Do/settings.py
+++ To_Do/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'To_Do.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'To_Do_List.db',
     }
 }
 
